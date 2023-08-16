10f68ca35ed4bf88325dba1b355d9ac103098ada
Ron256 <ejaluronaldlee@gmail.com>
2023-08-15 23:08:30 -0400

    adding Week5 directory files

---
+++ Week5/Reading tweets.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on ???
+
+@author: fitsstudiouser
+"""
+
+tweetData = 'http://rasinsrv07.cstcis.cti.depaul.edu/CSC455/tweet_data.txt'
+
+#fd = open('tweet_data.txt') # open local .txt file
+import urllib.request
+
+webFD = urllib.request.urlopen(tweetData)
+
+ln = webFD.readline()
+print(type(ln))
+
+# import json
+
+# tweet = json.loads(ln[6:])
+
+# tweet.keys()
+
+# tweet['created_at']
+# tweet['lang']
+# tweet['source']
+
+# tweetUser = tweet['user']
+
+# tweetUser['created_at']
+# tweet['user']['created_at']
+# tweet['created_at']
+
+# tweet['geo']
+# tweet['coordinates']
+
+
+
+
+
+
+
+
